8c62b8114cf9643eabca8c4d6a64a45fdcc1a22d
torreycoan <78583732+torreycoan@users.noreply.github.com>
2023-04-23 12:06:51 -0500

    premidterm lecture files

---
+++ 2-17rpc/client.py
@@ -0,0 +1,9 @@
+import grpc
+import math_pb2_grpc
+import math_pb2
+
+channel = grpc.insecure_channel("localhost:5444")
+stub = math_pb2_grpc.CalcStub(channel)
+
+response = stub.Mult(math_pb2.MultReq(x=3, y=4))
+print(response)
